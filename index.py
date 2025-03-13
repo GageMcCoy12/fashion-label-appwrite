24a55fb79c339c723aabbc97a06831121c2341ad
gage mccoy <75149938+GageMcCoy12@users.noreply.github.com>
2025-03-13 08:14:46 -0600

    Update index.py

--- index.py
+++ index.py
@@ -102,7 +102,10 @@ def main(context):
         5. Aesthetic/style (e.g., casual, formal, streetwear)
         6. Extra details (specific item or close alternative)
         7. Item Name (The name of the clothing item. Format in Title Case. Specify color of the item. If I search the item name I should be able to find the exact item I want.)
-        
+
+        Prioritize similar looks/styles over brand. If you don't know the exact item, provide a clothing item that brings the same style. It does NOT have to be from the same brand. 
+        (i.e. if you see a shirt that looks similar to an item from a different brand than you detected, then return that similar shirt instead.)
+
         Format response as a JSON array with:
         {
             "type": "",
